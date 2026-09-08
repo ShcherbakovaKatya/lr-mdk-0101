41035567fe2d691c4256d50c0851a5e3d669cc24
Y <@gmail.com>
2026-09-08 10:22:11 +0300

    Ввод 3х чисел

--- GitHub/GitHub/Program.cs
+++ GitHub/GitHub/Program.cs
@@ -10,7 +10,14 @@ namespace GitHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Введите первое число ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите третье число ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+
         }
     }
 }
