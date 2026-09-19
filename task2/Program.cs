c811c1081f13a610f492bad5f600f342cf19ab16
ShcherbakovaKatya <katy05792@gmail.com>
2026-09-19 11:28:19 +0300

    решение первой задачи

---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько элементов в массиве: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[a];
+
+            for (int i = 0; i<a; i++)
+            {
+                Console.WriteLine("Введите элемент массива: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach (int n in arr)
+                Console.Write(n + " ");
+        }
+    }
+}
